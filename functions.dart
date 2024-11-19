4d895ded941d38e5885f3d8cc2ff4fdfe35d30a0
EZZYY01 <Manish@mobcr3atives.com>
2024-11-19 09:29:13 +0545

    functions

---
+++ functions.dart
@@ -0,0 +1,13 @@
+void main() {
+  greetUser("Nikesh");
+  int sum = addNumber(5, 10);
+  print("Sum: $sum");
+}
+
+void greetUser(String name) {
+  print("Hello, $name!");
+}
+
+int addNumber(int a, int b) {
+  return a + b;
+}
