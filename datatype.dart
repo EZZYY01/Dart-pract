a357eb7d6acbc782aa11e6008fec5af36c567015
EZZYY01 <Manish@mobcr3atives.com>
2024-11-19 09:28:31 +0545

    conditional

---
+++ datatype.dart
@@ -0,0 +1,10 @@
+void main() {
+  int age = 22;
+  double height = 5.4;
+  String name = 'Nikesh';
+  bool isStudent = true;
+  var city = 'Kathmandu';
+
+  print(
+      'Name: $name, Age: $age,  Height: $height, City: $city, Student: $isStudent');
+}
